eadba53639db715f5b141213b40f98d558190709
praetinee <praetinee@gmail.com>
2025-08-28 14:37:15 +0700

    Update __init__.py

--- forms/__init__.py
+++ forms/__init__.py
@@ -1,16 +1,5 @@
 # praetinee/envoccdiseases/EnvOccDiseases-main/forms/__init__.py
 
-# This file tells Python what modules are available in the 'forms' package.
-# By importing them here, other parts of the application (like app.py)
-# can then import them directly from 'forms'.
-
-from . import lead_occupational
-from . import lead_occupational_medical
-from . import lead_env_adult_history
-from . import lead_env_adult_investigation
-from . import lead_env_child_history
-from . import lead_env_child_investigation
-from . import silicosis
-from . import confined_space
-from . import pesticide
-from . import pm25
+# This file can be empty.
+# Its presence tells Python that the 'forms' directory is a package.
+# We will handle the imports directly in app.py to avoid path issues.
